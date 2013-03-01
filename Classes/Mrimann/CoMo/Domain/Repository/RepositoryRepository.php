1704ea926b4da4e9786406ed7baa4b51ed49aa62
Mario Rimann <mario@rimann.org>
2013-03-01 08:44:39 +0100

    [FEATURE] Add a repository class for Git-Repositories

---
+++ Classes/Mrimann/CoMo/Domain/Repository/RepositoryRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace Mrimann\CoMo\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "Mrimann.CoMo".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A repository for Repositories
+ *
+ * @Flow\Scope("singleton")
+ */
+class RepositoryRepository extends \TYPO3\Flow\Persistence\Repository {
+
+	// add customized methods here
+
+}
+?>
