bea3f2f7abe5390cab6207d95def8edbfa3a70a3
Mario Rimann <mario@rimann.org>
2013-03-04 21:55:10 +0100

    [REFACTOR] Election CLI

--- Classes/Mrimann/CoMo/Command/ElectionCommandController.php
+++ Classes/Mrimann/CoMo/Command/ElectionCommandController.php
@@ -28,38 +28,86 @@ class ElectionCommandController extends \TYPO3\Flow\Cli\CommandController {
 	var $awardRepository;
 
 	/**
+	 * The best committers for an award that is being elected
+	 *
+	 * @var \Mrimann\CoMo\Domain\Model\AggregatedDataPerUser
+	 */
+	var $topCommitters;
+
+	/**
 	 * Runs the election for the last month.
 	 *
 	 * @return void
 	 */
 	public function electLastMonthCommand() {
-		// build last month's month-identifier
-		$date = mktime(1, 1, 1, date('m') - 1, 1, date('Y'));
-		$month = new \DateTime('@' . $date);
-		$monthIdentifier = $month->format('Y-m');
+		$monthIdentifier = $this->getMonthIdentifierLastMonth();
 
 		$this->outputLine('Getting the awards for ' . $monthIdentifier);
 
+		if ($this->canAwardBeElected('committerOfTheMonth', $monthIdentifier) === TRUE) {
+			$this->showTopRankingAndAnnouncement();
+			$this->createNewAward('committerOfTheMonth', $monthIdentifier, $this->topCommitters->getFirst());
+		} else {
+			return;
+		}
+
+	}
+
+	/**
+	 * Creates the identifier for last month in the format "YYYY-MM".
+	 *
+	 * @return string the month identifier
+	 */
+	protected function getMonthIdentifierLastMonth() {
+		$date = mktime(1, 1, 1, date('m') - 1, 1, date('Y'));
+		$month = new \DateTime('@' . $date);
+
+		return $month->format('Y-m');
+	}
+
+	/**
+	 * Checks if a given type of award for a specific month can be elected right now. This
+	 * decision bases on two facts:
+	 * - check if the award for this month is already given
+	 * - check if we have data for this month to vote on
+	 *
+	 * @param string the type of the award
+	 * @param string the month identifier
+	 * @return boolean true if the award can be elected, false otherwise
+	 */
+	protected function canAwardBeElected($type, $monthIdentifier) {
 		// first check if there is an award stored already
-		if ($this->awardRepository->findByMonthAndType($monthIdentifier, 'committerOfTheMonth')->count()) {
+		if ($this->awardRepository->findByMonthAndType($monthIdentifier, $type)->count()) {
 			$this->outputLine('This award has been given already.');
-			return;
+			return FALSE;
 		}
 
-		$hits = $this->electomat->getAwardsForMonth($monthIdentifier);
+		// check if we have data for this month at all
+		$this->topCommitters = $this->electomat->getAwardsForMonth($monthIdentifier);
 
-		if ($hits->count() === 0) {
-			$this->outputLine('Nothing found, sorry...');
-			return;
+		if ($this->topCommitters->count() === 0) {
+			$this->outputLine('Nothing found to elect on, sorry...');
+			return FALSE;
 		}
 
-		foreach ($hits as $hit) {
+		return TRUE;
+	}
+
+	/**
+	 * Shows the top committers for an award and also announces the very first one as the winner.
+	 *
+	 * This is solely for the CLI output - does no data manipulation.
+	 *
+	 * @return void
+	 */
+	protected function showTopRankingAndAnnouncement() {
+		foreach ($this->topCommitters as $topCommitter) {
 			$this->outputLine(
 				'%d commits from %s <%s>',
 				array(
-					$hit->getCommitCount(),
-					$hit->getUserName(),
-					$hit->getUserEmail()
+					$topCommitter->getCommitCount(),
+					$topCommitter->getUserName(),
+					$topCommitter->getUserEmail()
 				)
 			);
 		}
@@ -67,16 +115,25 @@ class ElectionCommandController extends \TYPO3\Flow\Cli\CommandController {
 		$this->outputLine(
 			'Happy to announce that the winner is: %s (%s) with %d commits!',
 			array(
-				$hits->getFirst()->getUserName(),
-				$hits->getFirst()->getUserEmail(),
-				$hits->getFirst()->getCommitCount()
+				$this->topCommitters->getFirst()->getUserName(),
+				$this->topCommitters->getFirst()->getUserEmail(),
+				$this->topCommitters->getFirst()->getCommitCount()
 			)
 		);
+	}
 
+	/**
+	 * Creates an award of a given type
+	 *
+	 * @param string the type of the award
+	 * @param string the month identifier
+	 * @param \Mrimann\CoMo\Domain\Model\AggregatedDataPerUser $winner
+	 */
+	protected function createNewAward($type, $monthIdentifier, \Mrimann\CoMo\Domain\Model\AggregatedDataPerUser $winner) {
 		$award = new \Mrimann\CoMo\Domain\Model\Award();
-		$award->setUserEmail($hits->getFirst()->getUserEmail());
-		$award->setUserName($hits->getFirst()->getUserName());
-		$award->setType('committerOfTheMonth');
+		$award->setUserEmail($winner->getUserEmail());
+		$award->setUserName($winner->getUserName());
+		$award->setType($type);
 		$award->setMonth($monthIdentifier);
 		$this->awardRepository->add($award);
 	}
