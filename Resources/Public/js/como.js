98fd6154e6f05c2751032cc280e0995d66de59b9
Mario Rimann <mario@rimann.org>
2013-04-16 23:41:14 +0200

    [TASK] Slider for the topic awards

---
+++ Resources/Public/js/como.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".topicAwardsSlider").bxSlider({
+		pager: false,
+		controls: false,
+		auto: true,
+		minSlides: 3,
+		maxSlides: 3,
+		moveSlides: 1,
+		slideWidth: 350
+	});
+});
