b09f2d5f07995f5ecb3cadfc2b14e01eb59918e0
Mario Rimann <mario@rimann.org>
2013-10-08 21:59:22 +0200

    [FEATURE] Auto-Reload the whole screen from time to time
    Fixes #18

--- Resources/Public/js/como.js
+++ Resources/Public/js/como.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+	// reload the whole page every 10min
+	window.setTimeout('reloadWholePage()', 600000);
+
 	$(".topicAwardsSlider").bxSlider({
 		pager: false,
 		controls: false,
@@ -9,3 +12,11 @@ $(document).ready(function() {
 		slideWidth: 350
 	});
 });
+
+/**
+ * Fades the page out a bit, then reloads the whole page
+ */
+function reloadWholePage() {
+	$('body div, body p').fadeTo('fast', 0.5);
+	location.reload();
+}
